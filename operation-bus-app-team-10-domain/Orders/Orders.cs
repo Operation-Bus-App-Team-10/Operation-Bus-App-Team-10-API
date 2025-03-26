e4ae4d50546ee1f8a0b37eadd95ba1655b9b4a95
Ben Dickerson <Dickerson.361@buckeyemail.osu.edu>
2025-03-26 10:44:22 -0400

    Updated Orderitem and Orders

---
+++ operation-bus-app-team-10-domain/Orders/Orders.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.ling;
+
+namespace Jet.Piranha.Domain.Orders
+{
+
+    public class Order
+    {
+        public int id{ get; set;}
+        public List<OrderItem> Items { get; set;}
+        public DateTime CreatedDate{ get; set;}
+        public decimal TotalPrice => Items.Sum(i => i.Price);
+
+    }
+}
