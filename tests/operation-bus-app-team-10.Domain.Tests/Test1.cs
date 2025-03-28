bf8d98a582f76e4950e9efd2ada7406be49019c9
Tarun Jarial <tarunjarial61@gmail.com>
2025-03-28 18:33:46 -0400

    Added RatingTests for invalid ratings

--- tests/operation-bus-app-team-10.Domain.Tests/Test1.cs
+++ tests/operation-bus-app-team-10.Domain.Tests/Test1.cs
@@ -1,10 +1,32 @@
-﻿namespace operation_bus_app_team_10.Domain.Tests;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using operation-bus-app-team-10.Domain;
 
-[TestClass]
-public sealed class Test1
+namespace operation-bus-app-team-10.Domain.Tests
 {
-    [TestMethod]
-    public void TestMethod1()
+    [TestClass]
+    public class RatingTests
     {
+        [TestMethod]
+        public void CreateRating_ValidStars_CreatesSuccessfully()
+        {
+            // Arrange
+            int validStars = 5;
+
+            // Act
+            var rating = new Rating(validStars);
+
+            // Assert
+            Assert.AreEqual(validStars, rating.Stars);
+        }
     }
+    [TestMethod]
+[ExpectedException(typeof(ArgumentException))]
+public void CreateRating_InvalidStars_ThrowsException()
+{
+    // Arrange
+    int invalidStars = -1;
+
+    // Act
+    var rating = new Rating(invalidStars);
+}
 }
